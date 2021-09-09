bb48b407ff89f42f4de6c723263f6af5c6c5f352
Juan <jmterzano@gmail.com>
2021-09-09 04:06:29 -0300

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidoComponent } from './page/bienvenido/bienvenido.component';
+import { ErrorComponent } from './page/error/error.component';
+import { LoginComponent } from './page/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'bienvenido',component:BienvenidoComponent},
+  {path:'login',component:LoginComponent},
+  //{path:'casa',component:BienvenidoComponent},
+  {path:'',redirectTo:'bienvenido',pathMatch:'full'},
+  {path:'**',component:ErrorComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenidoComponent } from './page/bienvenido/bienvenido.component';
+import { LoginComponent } from './page/login/login.component';
+import { ErrorComponent } from './page/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
